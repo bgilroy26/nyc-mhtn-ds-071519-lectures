7e702d34a1153e76241d374e93cb2da2f0b1397e
Brendan D. Gilroy <bdgilroy@gmail.com>
2019-07-18 10:04:21 -0400

    changes to week-1 content

---
+++ week-1/OOP/example_mod.py
@@ -0,0 +1,10 @@
+import random
+
+s = "Hey Class, how cool is this. I just brought in some stuff from a random python file"
+a = [100, 200, 300]
+
+def pick1(arg):
+    print(random.choice(arg))
+
+def pick2(arg):
+    pass
